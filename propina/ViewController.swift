c4e26bc8536839a8ad429f6202e8b84258d1636a
4n4rk0z <4n4rk0z@gmail.com>
2019-05-21 09:02:18 -0500

    first commit

---
+++ propina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  propina
+//
+//  Created by 4n4rk0z on 5/21/19.
+//  Copyright © 2019 4n4rk0z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
