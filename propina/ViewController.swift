a9f2be6cb4697dadd01ae8da7b9e081119701ff2
4n4rk0z <4n4rk0z@gmail.com>
2019-05-21 09:14:29 -0500

    add workspace

--- propina/ViewController.swift
+++ propina/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txfTotal: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
